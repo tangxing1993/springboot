eebfe1d2ef206de76fc7cd06f9cf646d592fc66d
tangxing <tangxingvip1993@gmail.com>
2017-08-08 17:56:36 +0800

    用户的控制类

---
+++ springboot-restful/src/main/java/org/tang/springboot/restful/controller/UserController.java
@@ -0,0 +1,87 @@
+package org.tang.springboot.restful.controller;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.tang.springboot.restful.entity.User;
+/**
+ * 
+ * @desc 用户的控制类 
+ * @date 2017年8月8日
+ * @author tx
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	/**
+	 * 创建线程安全的用户表
+	 */
+	static Map<Long,User> users = Collections.synchronizedMap(new HashMap<Long,User>());
+	
+	private static final Logger logger =  LoggerFactory.getLogger(UserController.class);
+	/**
+	 * 获取用户列表属性
+	 * @return
+	 */
+	@GetMapping("/")
+	public List<User> getUserList(){
+		logger.debug(" Get users ! ");
+		return new ArrayList<>(users.values());
+	}
+	/**
+	 * 添加用户
+	 * @param user
+	 * @return
+	 */
+	@PostMapping("/")
+	public String postUser(@ModelAttribute User user){
+		users.put(user.getId(), user);
+		return "success";
+	}
+	/**
+	 * 获取指定id的用户信息
+	 * @param id
+	 * @return
+	 */
+	@GetMapping("/{id}")
+	public User getUserById(@PathVariable("id") Long id){
+		return users.get(id);
+	}
+	/**
+	 * 修改用户信息
+	 * @param id
+	 * @param user
+	 * @return
+	 */
+	@PutMapping("/{id}")
+	public String putUser(@PathVariable("id") Long id,@ModelAttribute User user){
+		User user2 = users.get(id);
+		user2.setName(user.getName());
+		user2.setAge(user.getAge());
+		return "success";
+	}
+	/**
+	 * 删除用户信息
+	 * @param id
+	 * @return
+	 */
+	@DeleteMapping("/{id}")
+	public String deleteUser(@PathVariable("id") Long id){
+	    users.remove(id);
+		return "success";
+	}
+	
+}
