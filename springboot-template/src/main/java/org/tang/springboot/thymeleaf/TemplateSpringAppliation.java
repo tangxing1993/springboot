3f166b15f2938c23676d8992ac0e1e23882fd09c
tangxing <tangxingvip1993@gmail.com>
2017-08-09 17:14:05 +0800

    springboot-template的启动程序

---
+++ springboot-template/src/main/java/org/tang/springboot/thymeleaf/TemplateSpringAppliation.java
@@ -0,0 +1,19 @@
+package org.tang.springboot.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot的启动程序
+ * @desc 
+ * @date 2017年8月9日
+ * @author tx
+ */
+@SpringBootApplication
+public class TemplateSpringAppliation {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(TemplateSpringAppliation.class, args);
+	}
+	
+}
