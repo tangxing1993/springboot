db83fbc28a7d0e9655c0a02562a02819ff94f4cc
tangxing <tangxingvip1993@gmail.com>
2017-08-09 14:36:56 +0800

    springboot-config的spring主程序

---
+++ springboot-config/src/test/java/org/tang/springboot/config/ConfigSpringApplication.java
@@ -0,0 +1,18 @@
+package org.tang.springboot.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * springboot的配置文件学习
+ * @desc 
+ * @date 2017年8月9日
+ * @author tx
+ */
+@SpringBootApplication
+public class ConfigSpringApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ConfigSpringApplication.class, args);
+	}
+
+}
