ab107cdd528bbb74643d2096027422ce4f22f0f5
tangxing <tangxingvip1993@gmail.com>
2017-08-10 10:46:53 +0800

    用户的实体类

---
+++ springboot-swagger2/src/main/java/org/tang/springboot/swagger2/entity/User.java
@@ -0,0 +1,37 @@
+package org.tang.springboot.swagger2.entity;
+
+import java.io.Serializable;
+/**
+ * 用户的实体类
+ * @desc 
+ * @date 2017年8月9日
+ * @author tx
+ */
+public class User implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+	private String age;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+	
+}
