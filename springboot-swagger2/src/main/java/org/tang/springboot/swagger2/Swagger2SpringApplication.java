754d1883932c9160044116c59ef8bd53e8026d27
tangxing <tangxingvip1993@gmail.com>
2017-08-10 10:43:59 +0800

    Swagger的springboot的启动程序

---
+++ springboot-swagger2/src/main/java/org/tang/springboot/swagger2/Swagger2SpringApplication.java
@@ -0,0 +1,17 @@
+package org.tang.springboot.swagger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @desc  springboot的启动程序 
+ * @date 2017年8月9日
+ * @author tx
+ */
+@SpringBootApplication
+public class Swagger2SpringApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Swagger2SpringApplication.class, args);
+	}
+	
+}
