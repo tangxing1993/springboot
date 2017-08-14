b373382af9cb97b4ae463f5b740b2b6e6f504756
tangxing <tangxingvip1993@gmail.com>
2017-08-14 17:04:35 +0800

    springboot应用程序

---
+++ springboot-jpa/src/main/java/org/tang/springboot/jpa/JpaSpringApplication.java
@@ -0,0 +1,19 @@
+package org.tang.springboot.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 唐兴
+ * @date   2017年8月14日
+ * @desc  springboot的启动程序
+ */
+@SpringBootApplication
+public class JpaSpringApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(JpaSpringApplication.class, args);
+	}
+
+	
+}
