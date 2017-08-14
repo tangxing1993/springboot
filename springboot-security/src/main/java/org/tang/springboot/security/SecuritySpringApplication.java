519489167220aa3769515d7f6b6e750a6f50cd9f
tangxing <tangxingvip1993@gmail.com>
2017-08-14 14:07:48 +0800

    springboot的启动程序

---
+++ springboot-security/src/main/java/org/tang/springboot/security/SecuritySpringApplication.java
@@ -0,0 +1,18 @@
+package org.tang.springboot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @desc  SpringBoot的启动程序 
+ * @date 2017年8月10日
+ * @author tx
+ */
+@SpringBootApplication
+public class SecuritySpringApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SecuritySpringApplication.class, args);
+	}
+
+}
