7c67fedfe764556ce90a18aa043ccf41b5bd65e5
tangxing <tangxingvip1993@gmail.com>
2017-08-16 13:34:47 +0800

    启动程序

---
+++ springboot-multi-datasource/src/main/java/org/tang/springboot/multi/datasource/MultiDataSourceSpringApplication.java
@@ -0,0 +1,18 @@
+package org.tang.springboot.multi.datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 唐兴
+ * @date   2017年8月14日
+ * @desc   springboot启动程序
+ */
+@SpringBootApplication
+public class MultiDataSourceSpringApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(MultiDataSourceSpringApplication.class, args);
+	}
+	
+}
