b811537d54a75bcd2acf6f91533644b55902038c
tangxing <tangxingvip1993@gmail.com>
2017-08-29 11:09:11 +0800

    应用程序

---
+++ springboot-mybatis/src/main/java/org/tang/springboot/mybatis/MybatisSpringApplication.java
@@ -0,0 +1,17 @@
+package org.tang.springboot.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author 唐兴
+ * @date   2017年8月28日
+ * @desc   项目的启动程序
+ */
+@SpringBootApplication
+public class MybatisSpringApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(MybatisSpringApplication.class, args);
+	}
+	
+}
